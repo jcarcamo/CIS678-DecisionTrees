25abfc1728bf887003f284f6eba3da7aaa87eb55
Juan Gonzalo Carcamo <jcarcamo@gmail.com>
2017-03-09 20:07:31 -0500

    Modified .gitignore to remove xcode project files

---
+++ DecisionTrees/DecisionTrees/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DecisionTrees
+//
+//  Created by Juan Gonzalo Carcamo on 3/9/17.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
